1bcecec39595e7b7dc7760857e01b01d4810020a
vrushabhp <vrushabh@albot.io>
2021-12-24 19:51:00 +0530

    width

--- src/components/Result.js
+++ src/components/Result.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Result({ score, playAgain }) {
     return (
-        <div className='score-board'>
+        <div className='score-board container'>
             <div className='score'>You Scored {score} / 5 correct answers...!</div>
             <button className='playBtn' onClick={playAgain}>Play Again</button>
         </div>
