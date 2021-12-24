63876c2c5ee45a6c29779af508b453889dab3e36
vrushabhp <vrushabh@albot.io>
2021-12-24 18:47:25 +0530

    first

---
+++ src/components/QuizBee.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import "../Assets/Style.css"
+import quizService from '../quizService/index'
+import QuestionBox from './QuestionBox'
+import Result from './Result'
+export class QuizBee extends Component {
+    state = {
+        questionBank: [],
+        score: 0,
+        responses: 0
+    }
+    getQuestions = () => {
+        quizService().then(question => {
+            this.setState({
+                questionBank: question
+            })
+        })
+    }
+    componentDidMount() {
+        this.getQuestions();
+    }
+    computerAnswer = (answer, correctAnswer) => {
+        if (answer === correctAnswer) {
+            this.setState({
+                score: this.state.score + 1
+            })
+        }
+
+        this.setState({
+            responses: this.state.responses < 5 ? this.state.responses + 1 : 5
+        })
+
+    }
+    playAgain = () => {
+        this.getQuestions();
+        this.setState({
+            score: 0,
+            responses: 0
+        })
+    }
+    render() {
+        return (
+            <div className='container'>
+                <div className='title'>Quiz Game</div>
+                {
+                    this.state.questionBank.length > 0 &&
+                    this.state.responses < 5 &&
+                    this.state.questionBank.map(
+                        ({ question, answers, correct, questionId }) => (
+                            <QuestionBox
+                                question={question}
+                                options={answers}
+                                key={questionId}
+                                selected={answer => this.computerAnswer(answer, correct)}
+                            />))
+                }
+                {
+                    this.state.responses === 5 ? (<Result score={this.state.score} playAgain={this.playAgain} />) : null
+                }
+            </div>
+        )
+    }
+}
+
+export default QuizBee
